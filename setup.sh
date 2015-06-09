c94c35e880318f3be1535522a75d3d83797c124d
Júlio L. Jr <julio.leitao.jr@gmail.com>
2015-06-08 21:59:47 -0300

    Adicionando arquivo de setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install npm
+sudo npm install -g ionic
+sudo npm install -g cordova
+echo "Baixando android sdk"
+wget https://dl.google.com/android/android-sdk_r24.2-linux.tgz
