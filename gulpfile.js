116d79cb70a724a093b317c25f9e2837a0cdaa8d
Wesley <wesley.torres@ccc.ufcg.edu.br>
2015-06-10 18:13:31 -0300

    interface do login e signup

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/*.scss']
 };
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  gulp.src('./scss/style.app.scss')
     .pipe(sass({
       errLogToConsole: true
     }))
