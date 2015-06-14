0e7e7fb70c27ca645d95c633a10ba10ecbc96957
Júlio L. Jr <julio.leitao.jr@gmail.com>
2015-06-14 00:17:36 -0300

    Adicionando menu inicial e som

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@ var firebaseUrl = "https://trunfo-les.firebaseio.com/";
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','firebase'])
+angular.module('starter', ['ionic','firebase', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers/MenuController.js
+++ www/js/controllers/MenuController.js
@@ -1,9 +1,44 @@
 var app = angular.module('starter');
 
-app.controller('MenuController', function($scope, $state) {
+app.controller('MenuController', function($scope, $state, $cordovaMedia, $ionicLoading) {
 
-    $scope.logout = function() {
-        //UserService.clear();
+    self = this;
+
+    $scope.batalhar = function() {
+        console.log("SALA DE BATALHA");
+        self.clickSound();
+    };
+
+    $scope.army = function() {
+        console.log("ARMY");
+        self.clickSound();
+    };
+
+    $scope.shop = function() {
+        console.log("SHOP");
+        self.clickSound();
+    };
+
+    $scope.credits = function() {
+        console.log("CREDITS");
+        self.clickSound();
+    };
+
+    $scope.config = function() {
+        console.log("CONFIG");
+        self.clickSound();
+    };
+
+    $scope.exit = function() {
+        self.clickSound();
         $state.go('login');
+        ionic.Platform.exitApp();
+    };
+
+    this.clickSound = function() {
+        var media = $cordovaMedia.newMedia("../../sound/click.mp3").then(function() {
+        }, function() {
+        });
+        media.play(media);
     };
 });
