3c6fc5ffb4959ce9f549864cf7dd77c7af052c8c
Júlio L. Jr <julio.leitao.jr@gmail.com>
2015-06-17 00:04:03 -0300

    Alterando menu responsivo e audio

--- www/js/controllers/MenuController.js
+++ www/js/controllers/MenuController.js
@@ -1,44 +1,31 @@
 var app = angular.module('starter');
 
-app.controller('MenuController', function($scope, $state, $cordovaMedia, $ionicLoading) {
+app.controller('MenuController', function($scope, $state) {
 
-    self = this;
+    self = this
 
     $scope.batalhar = function() {
         console.log("SALA DE BATALHA");
-        self.clickSound();
     };
 
     $scope.army = function() {
         console.log("ARMY");
-        self.clickSound();
     };
 
     $scope.shop = function() {
         console.log("SHOP");
-        self.clickSound();
     };
 
     $scope.credits = function() {
         console.log("CREDITS");
-        self.clickSound();
     };
 
     $scope.config = function() {
         console.log("CONFIG");
-        self.clickSound();
     };
 
     $scope.exit = function() {
-        self.clickSound();
-        $state.go('login');
-        ionic.Platform.exitApp();
-    };
-
-    this.clickSound = function() {
-        var media = $cordovaMedia.newMedia("../../sound/click.mp3").then(function() {
-        }, function() {
-        });
-        media.play(media);
+        document.getElementById('clicado').play();
+       // $state.go('login');
     };
 });
