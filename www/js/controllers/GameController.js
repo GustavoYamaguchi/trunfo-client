62f8514b3b3d78b7b4ec6c61e72f5caf404bf3a1
Júlio L. Jr <julio.leitao.jr@gmail.com>
2015-07-01 19:03:04 -0300

    Adicionando batalha simples

--- www/js/controllers/GameController.js
+++ www/js/controllers/GameController.js
@@ -1,8 +1,19 @@
 var app = angular.module('starter');
 
 /**
- *
+ * Controller para sala de batalha.
  */
 app.controller('GameController', function ($scope, $state) {
+	
+	self = this;
 
+	$scope.path = 'img/cards/';
+
+	$scope.card = function() {
+		return cartas[0];
+	};
+
+	$scope.another = function() {
+		return cartas[1];
+	};
 });
