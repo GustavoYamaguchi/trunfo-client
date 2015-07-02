402a56059a0b47b21ad53b2efdbac402fcc7be12
Wesley Nunes <wesley.nmtorres@gmail.com>
2015-07-01 21:09:57 -0300

    menu com musica no background funcionando

--- www/js/controllers/MenuController.js
+++ www/js/controllers/MenuController.js
@@ -1,12 +1,20 @@
 var app = angular.module('starter');
 
-app.controller('MenuController', function($scope,$cordovaMedia, $state) {
+app.controller('MenuController', function($scope,$state) {
 
     self = this
+    self.media = null;
 
-    $scope.play = function(src) {
-        var media = new window.Media(src, null, null, function(){});
-        $cordovaMedia.play(media);
+    $scope.play = function() {
+        self.media = new Media("/android_asset/www/sound/menu.mp3", function () {
+            console.log("playAudio():Audio Success");
+        },
+        function (err) {
+            console.log("playAudio():Audio Error: " );
+            console.log(err);
+        }
+        );
+        self.media.play();
     }
  
 
