5302ac990d2cf2278327a1c4a822820718b28d91
Wesley <wesley.nmtorres@gmail.com>
2015-07-10 10:29:18 -0300

    menu responsívo e com som

--- www/js/controllers/MenuController.js
+++ www/js/controllers/MenuController.js
@@ -19,40 +19,32 @@ app.controller('MenuController', function($scope,$state) {
  
 
     $scope.batalhar = function() {
-        clickSound();
+        
         $state.go('game');
     };
 
     $scope.army = function() {
-        clickSound();
+        
         console.log("ARMY");
     };
 
     $scope.shop = function() {
-        clickSound();
+        
         console.log("SHOP");
     };
 
     $scope.credits = function() {
-        clickSound();
+        
         console.log("CREDITS");
     };
 
     $scope.config = function() {
-        clickSound();
+        
         console.log("CONFIG");
     };
 
     $scope.exit = function() {
-        clickSound();
+        
         $state.go('login');
-    };
-
-    /**
-     * Click sound execute when the user
-     * tap at button menu
-     */
-    function clickSound() {
-        document.getElementById('clicado').play();
     };
 });
